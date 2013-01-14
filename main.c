c7721330302a0c5b0b7291388142051f1f1db864
maxime lavandier <lavand_m@pc-lavand_m.(none)>
2013-01-14 17:21:53 +0100

    add main.c

---
+++ main.c
@@ -0,0 +1,9 @@
+/*
+** main.c for corewar in /home/lavand_m//projet/actu/corewar
+**
+** Made by maxime lavandier
+** Login   <lavand_m@epitech.net>
+**
+** Started on  Mon Jan 14 17:21:34 2013 maxime lavandier
+** Last update Mon Jan 14 17:21:35 2013 maxime lavandier
+*/
