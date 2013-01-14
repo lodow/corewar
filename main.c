5b47ce072c2a4c5fdedf3bc1f887b2f4fec2aa86
maxime lavandier <lavand_m@pc-lavand_m.(none)>
2013-01-14 17:27:12 +0100

    add func main()

--- main.c
+++ main.c
@@ -5,5 +5,10 @@
 ** Login   <lavand_m@epitech.net>
 **
 ** Started on  Mon Jan 14 17:21:34 2013 maxime lavandier
-** Last update Mon Jan 14 17:21:35 2013 maxime lavandier
+** Last update Mon Jan 14 17:24:43 2013 maxime lavandier
 */
+
+int	main(int count, char **v)
+{
+  printf ("coucou\n");
+}
