6734f4707e7c07bdffe2e604bfdec36c28ceb009
maxime lavandier <lavand_m@pc-lavand_m.(none)>
2013-01-16 13:04:38 +0100

    preparation du parse du fichier

--- asm/main.c
+++ asm/main.c
@@ -5,10 +5,23 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Mon Oct  8 16:20:21 2012 hugues morisset
+** Last update Wed Jan 16 13:03:07 2013 maxime lavandier
 */
+
+#include "../misc/op.h"
+#include "../misc/get_file.h"
 
 int	main(int argc, char **argv, char **envp)
 {
-  return (0);
+  int	fd;
+  char	**file;
+
+  if (argc < 2 && ((fd = open(file, O_RDONLY)) == -1))
+    {
+      my_putstr("file error\n");
+      return (0);
+    }
+  file = get_data_ffile(fd);
+  if (parse(file) == -1)
+    my_putstr("parse error\n");
 }
---
+++ asm/parce.c
@@ -0,0 +1,14 @@
+/*
+** parce.c for corewar in /home/lavand_m//projet/actu/corewar/asm
+**
+** Made by maxime lavandier
+** Login   <lavand_m@epitech.net>
+**
+** Started on  Wed Jan 16 13:02:05 2013 maxime lavandier
+** Last update Wed Jan 16 13:02:37 2013 maxime lavandier
+*/
+
+int	parse(char **file)
+{
+  return (-1);
+}
