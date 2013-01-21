992b15c08701c7ddf25de77bcd6d5c8e95acaa09
Tastyep <luc.sinet@epitech.eu>
2013-01-21 16:06:19 +0100

    Checked the norme on the file op.h

--- misc/op.h
+++ misc/op.h
@@ -5,7 +5,7 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Mon Oct  8 16:20:21 2012 hugues morisset
+** Last update Mon Jan 21 16:04:53 2013 luc sinet
 */
 
 #ifndef _OP_H_
@@ -13,83 +13,81 @@
 
 # define MEM_SIZE		(6 * 1024)
 # define IDX_MOD		512
+
 /*
 ** Registres
 */
+
 # define REG_NUMBER		16
+
 /*
 ** size in Bytes
 */
+
 # define IND_SIZE		2
 # define REG_SIZE		4
 # define DIR_SIZE		REG_SIZE
+
 /*
 ** Cycle things
 */
-# define CYCLE_TO_DIE	1536
-# define CYCLE_DELTA	5
+
+# define CYCLE_TO_DIE		1536
+# define CYCLE_DELTA		5
 # define NBR_LIVE		40
 
-# define MAX_ARGS_NUMBER         4     /* this may not be changed 2^*IND_SIZE */
+# define MAX_ARGS_NUMBER	4     /* this may not be changed 2^*IND_SIZE */
 
-#define COMMENT_CHAR            '#'
-#define LABEL_CHAR              ':'
-#define DIRECT_CHAR             '%'
-#define SEPARATOR_CHAR          ','
+# define COMMENT_CHAR		'#'
+# define LABEL_CHAR		':'
+# define DIRECT_CHAR		'%'
+# define SEPARATOR_CHAR		','
 
-#define LABEL_CHARS             "abcdefghijklmnopqrstuvwxyz_0123456789"
+# define LABEL_CHARS             "abcdefghijklmnopqrstuvwxyz_0123456789"
 
-#define NAME_CMD_STRING         ".name"
-#define COMMENT_CMD_STRING      ".comment"
+# define NAME_CMD_STRING         ".name"
+# define COMMENT_CMD_STRING      ".comment"
 
 /*
- **
- */
+** typedef char	args_type_t;
+*/
 
-typedef char    args_type_t;
+# define T_REG			1       /* registre */
+# define T_DIR			2       /* directe  (ld  #1,r1  met 1 dans r1) */
+# define T_IND			4       /* indirecte toujours relatif
+					** ( ld 1,r1 met ce qu'il y a l'adress (1+pc)
+					** dans r1 (4 octecs ))
+					*/
+# define T_LAB			8       /* LABEL */
 
-#define T_REG           1       /* registre */
-#define T_DIR           2       /* directe  (ld  #1,r1  met 1 dans r1) */
-#define T_IND           4       /* indirecte toujours relatif
-( ld 1,r1 met ce qu'il y a l'adress (1+pc)
-  dans r1 (4 octecs )) */
-#define T_LAB           8       /* LABEL */
+/*
+** header
+*/
 
-struct  op_s
+# define PROG_NAME_LENGTH        128
+# define COMMENT_LENGTH          2048
+# define COREWAR_EXEC_MAGIC      0xea83f3
+
+/*
+** extern op_t	g_op_tab[];
+*/
+
+typedef struct	op_s
 {
-  char         *mnemonique;
-  char         nbr_args;
-  args_type_t  type[MAX_ARGS_NUMBER];
-  char         code;
-  int          nbr_cycles;
-  char         *comment;
-};
+  char		*mnemonique;
+  char		nbr_args;
+  args_type_t	type[MAX_ARGS_NUMBER];
+  char		code;
+  int		nbr_cycles;
+  char		*comment;
+}		op_t;
 
-typedef struct op_s     op_t;
-
-/*
- ** op_tab
- */
-
-
-extern  op_t    op_tab[];
-
-
-/*
- ** header
- */
-#define PROG_NAME_LENGTH        128
-#define COMMENT_LENGTH          2048
-
-struct header_s
+typedef struct	header_s
 {
-  int  magic;
-#define COREWAR_EXEC_MAGIC      0xea83f3        /* why not */
-  char prog_name[PROG_NAME_LENGTH + 1];
-  int  prog_size;
-  char comment[COMMENT_LENGTH + 1];
-};
-
-typedef struct header_s header_t;
+  int		magic;
+  char		prog_name[PROG_NAME_LENGTH + 1];
+  int		prog_size;
+  char		comment[COMMENT_LENGTH + 1];
+}		header_t;
 
 #endif
