1fb36fb2d47e0ebd2d63fed65078c0bc8f6303d0
Adrien <dmadrien@msn.com>
2013-01-23 15:13:01 +0100

    Add func addlabel and some variable in t_cmd

--- asm/parse_cmd.h
+++ asm/parse_cmd.h
@@ -5,7 +5,7 @@
 ** Login   <lavand_m@epitech.net>
 **
 ** Started on  Wed Jan 16 14:20:12 2013 maxime lavandier
-** Last update Tue Jan 22 16:12:56 2013 maxime lavandier
+** Last update Wed Jan 23 14:38:37 2013 Welanor
 */
 
 #ifndef PARSE_CMD
@@ -16,13 +16,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef	struct	s_tablabel
+{
+  char		*label;
+  int		adress;
+}		t_tablabel;
+
 typedef struct	s_cmd
 {
-  char	*opt_label;
-  char	*opocode;
-  char	*param[MAX_ARGS_NUMBER + 1];
-  void	*file;
-  int	pc;
+  char		*opt_label;
+  char		*opocode;
+  char		*param[MAX_ARGS_NUMBER + 1];
+  void		*file;
+  int		pc;
+  int		lablengh;
+  t_tablabel	*lab;
 }		t_cmd;
 
 int	parse_cmd(char *line, t_header *header, t_cmd *cmd);
