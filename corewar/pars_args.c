d6269aea49218e1e9d7b943d6db2a28b58a09254
Tastyep <luc.sinet@epitech.eu>
2013-01-23 20:49:11 +0100

    changed name from pars_args to pars and created a pars_args file :D finished to check the args, started to pars them.

--- corewar/pars_args.c
+++ corewar/pars_args.c
@@ -4,66 +4,7 @@
 ** Made by luc sinet
 ** Login   <sinet_l@epitech.net>
 **
-** Started on  Tue Jan 22 15:41:27 2013 luc sinet
-** Last update Wed Jan 23 20:41:21 2013 luc sinet
+** Started on  Wed Jan 23 20:47:09 2013 luc sinet
+** Last update Wed Jan 23 20:47:11 2013 luc sinet
 */
-
-#include "include.h"
-
-void	error_msg(int type)
-{
-  if (type == 0)
-    my_putstr("Error, please verifie your arguments\n"
-              "./corewar [-dump nbr_cycle][[-n prog_number]"
-              "[-a load_address ] prog_name]\n", 1, -1);
-}
-
-int	check_args(char **av, int nb_arg)
-{
-  int	i;
-  int	error;
-
-  i = 0;
-  while (i < nb_arg)
-    {
-      if ((error = check_opt(av, av[i], &i, nb_arg)) < 0)
-        {
-          if (error == -2 || check_champ(av[i]) == -1)
-            {
-              error_msg(0);
-              return (-1);
-            }
-        }
-      i += 1;
-    }
-  return (0);
-}
-
-int	pars_args(char **av, int nb_arg)
-{
-  int	i;
-
-  i = 0;
-  while (i < nb_args)
-    {
-      if (pars_opt(av, &i, nb_arg) < 0)
-	{
-	  if (pars_champ(av[i]) == -1)
-	    {
-	      error_msg(1);
-	      return (-1);
-	    }
-	}
-    }
-  return (0);
-}
-
-int	pars(char **av, int ac)
-{
-  if (ac == 0 || check_args(av, ac) == -1)
-    return (-1);
-  if (pars_args(av, ac) == -1)
-    return (-1);
-  return (0);
-}
 
