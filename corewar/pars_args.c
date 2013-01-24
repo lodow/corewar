3f41262c150890425aa44e3a6ca02ad9807967aa
Tastyep <luc.sinet@epitech.eu>
2013-01-24 11:55:19 +0100

    started the parsing of options

--- corewar/pars_args.c
+++ corewar/pars_args.c
@@ -5,6 +5,47 @@
 ** Login   <sinet_l@epitech.net>
 **
 ** Started on  Wed Jan 23 20:47:09 2013 luc sinet
-** Last update Wed Jan 23 20:47:11 2013 luc sinet
+** Last update Thu Jan 24 11:54:46 2013 luc sinet
 */
 
+#include "include.h"
+
+void	dump_opt(int nb_cycle)
+{
+
+}
+
+void	prog_num_opt(int nb)
+{
+
+}
+
+void	load_addr_opt(int addr)
+{
+
+}
+
+int	pars_opt(char **av, int *pos, int max)
+{
+  int	i;
+  char	*opt_list[3];
+
+  i = 0;
+  opt_list[0] = "dump";
+  opt_list[1] = "-n";
+  opt_list[2] = "-a";
+  while (i < 3 && my_strcmp(opt_list[i], av[*pos]) != 0)
+    i++;
+  if (i == 0)
+    dump_opt(my_get_nbr(av[*pos + 1]));
+  else if (i == 1)
+    prog_num_opt(my_get_nbr(av[*pos + 1]));
+  else
+    {
+      if (*pos + 1 == max)
+	load_addr_opt(-1);
+      else
+	load_addr_opt(my_get_nbr(av[*pos + 1]));
+    }
+  return (0);
+}
