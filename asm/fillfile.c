1b6117543c9503317bb65c76f6e727a1fdf6b7f9
Adrien <dmadrien@msn.com>
2013-01-25 12:10:14 +0100

    Le magic est dans le bon sens

--- asm/fillfile.c
+++ asm/fillfile.c
@@ -5,12 +5,13 @@
 ** Login   <adrien dellamaggiora@epitech.eu>
 **
 ** Started on  Thu Jan 24 16:58:24 2013 adrien dellamaggiora
-** Last update Fri Jan 25 11:37:38 2013 Welanor
+** Last update Fri Jan 25 12:08:49 2013 Welanor
 */
 
 #include "asm.h"
 #include "parse_cmd.h"
 #include "../misc/op.h"
+/*#include "../misc/str_func.c"*/
 
 extern	t_op	op_tab[];
 
@@ -63,12 +64,14 @@ void	fillfile(t_header *header, t_cmd *cmd, char *name)
 {
   int	fd;
   int	flag;
+  int	magic;
 
   flag = (O_CREAT & ~S_IRUSR & ~S_IWUSR & ~S_IRGRP & ~S_IROTH) | O_WRONLY;
   header->prog_size = cmd->pc;
   if ((fd = open(getname(name), flag)) == -1)
     exit(0);
-  header->magic = COREWAR_EXEC_MAGIC;
+  magic = COREWAR_EXEC_MAGIC;
+  header->magic = *((int*)switch_endian((char*)&(magic), sizeof(int)));
   my_putstr((char*)header, fd, sizeof(t_header));
   my_putstr(cmd->file, fd, cmd->sizefile);
   return ;
