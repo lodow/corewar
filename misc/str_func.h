2d48313634dc8cfb87250ff259992ba970a2e73d
Tastyep <luc.sinet@epitech.eu>
2013-01-27 17:17:51 +0100

    Added a memset for int array, included the prototype

--- misc/str_func.h
+++ misc/str_func.h
@@ -5,7 +5,7 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Thu Jan 24 13:47:12 2013 Welanor
+** Last update Sun Jan 27 17:11:43 2013 luc sinet
 */
 
 #ifndef _STR_FUNC_H_
@@ -19,7 +19,8 @@ void	my_putstr(char *str, int fd, int strlen);
 char	*my_strncpy(char *dest, char *src, int n);
 int	my_strcmp(char *s1, char *s2);
 int	my_begincmp(char *str1, char *str2);
-void	my_memset(void *ptr, int osize, char value);
+void	*my_memsetc(void *ptr, int size, char value);
+void	*my_memseti(void *ptr, int size, char value);
 char	*my_strdup(char *str);
 char	*switch_endian(char *var, int varsize);
 
