53ff8e500599ea6e09741d525a07eefe5f6321b0
Tastyep <luc.sinet@epitech.eu>
2013-01-27 17:16:46 +0100

    les espacements ...

--- corewar/vmstats.h
+++ corewar/vmstats.h
@@ -5,7 +5,7 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Mon Oct  8 16:20:21 2012 hugues morisset
+** Last update Sun Jan 27 17:16:27 2013 luc sinet
 */
 
 #ifndef _VMSTATS_H_
@@ -17,7 +17,7 @@
 # define VM_MEM(x) vm->mem[MOD_MEM((x))]
 # define GET_TYPE_PARAMX(x, y) (((x) >> ((MAX_ARGS_NUMBER - (y)) * 2)) & 0x11)
 
-typedef struct		s_champ
+typedef struct	s_champ
 {
   char		*freeme;
   t_header	header;
@@ -27,42 +27,41 @@ typedef struct		s_champ
 }		t_champ;
 
 typedef char	t_vmmem;
-
 typedef char	t_reg[REG_SIZE];
 
 typedef struct	s_process
 {
-  int	pc;
-  int	carry;
+  int		pc;
+  int		carry;
   t_champ	*associated_champ;
-  t_reg	reg[REG_NUMBER];
-  int	nb_cycle_t_next;
-}	t_process;
+  t_reg		reg[REG_NUMBER];
+  int		nb_cycle_t_next;
+}		t_process;
 
 typedef struct	s_param
 {
-  char	type;
-  int	size;
-  char	*value;
-}	t_param;
+  char		type;
+  int		size;
+  char		*value;
+}		t_param;
 
 typedef struct	s_vm
 {
-  int	instr_nb_cycle[16];
-  int	(*f[16])(t_process *proc, struct s_vm *vm);
+  int		instr_nb_cycle[16];
+  int		(*f[16])(t_process *proc, struct s_vm *vm);
   t_champ	**champs;
   t_list	*process_list;
   t_vmmem	*mem;
-  int	cycle_count;
-  int	nbr_live;
-  int	cycle_to_die;
-  int	cycle_to_dump;
+  int		cycle_count;
+  int		nbr_live;
+  int		cycle_to_die;
+  int		cycle_to_dump;
 }		t_vm;
 
 int		delete_process(void *ptr);
-t_champ	**add_champ_t_tab(t_champ **tab, t_champ *add);
-t_vm	*init_vm_stats(int cycle_t_die, int cycle_t_dump, t_vm *vm);
-t_champ	*load_champ(int fd, int number);
+t_champ		**add_champ_t_tab(t_champ **tab, t_champ *add);
+t_vm		*init_vm_stats(int cycle_t_die, int cycle_t_dump, t_vm *vm);
+t_champ		*load_champ(int fd, int number);
 int		op_param_size(char type);
 t_process	*up_champ_t_mem(t_vmmem *mem, t_champ *champ, int pc);
 t_process	*create_new_process(t_process *src, int pc);
