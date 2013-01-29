f66c07831a8c6bb42750b92637bed52e346c889f
Tastyep <luc.sinet@epitech.eu>
2013-01-29 16:50:23 +0100

    just added a toggle value function and applied it where the carry needed to be changed

--- corewar/op/op_lldi.c
+++ corewar/op/op_lldi.c
@@ -5,12 +5,13 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Mon Oct  8 16:20:21 2012 hugues morisset
+** Last update Tue Jan 29 16:44:46 2013 luc sinet
 */
 
 #include	"operation.h"
 
 int	op_lldi(t_process *proc, t_vm *vm)
 {
+  proc->carry = toggle_val(proc_carry);
   return (1);
 }
