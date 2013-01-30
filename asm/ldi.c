dc21a0f9d2ad44a0639d6fba7019d2866349ae49
Adrien <dmadrien@msn.com>
2013-01-30 18:04:06 +0100

    Add zjump ldi sti

---
+++ asm/ldi.c
@@ -0,0 +1,46 @@
+/*
+** ldi.c for ldi in /home/dellam_a/Projet/corewar/asm
+** 
+** Made by Adrien
+** Login   <dellam_a@epitech.eu>
+** 
+** Started on  Wed Jan 30 16:55:04 2013 Adrien
+** Last update Wed Jan 30 17:51:58 2013 Welanor
+*/
+
+int	next(char str, int i)
+{
+  while (str[i] != SEPARATOR_CHAR && str[i] != 0)
+    i++;
+  if (str[i] == 0)
+    return (-1);
+  i++;
+  return (i);
+}
+
+int	ldi_lldi(char *str, int i, t_param *param, t_cmd *cmd)
+{
+  param.param[0] = 0xa;
+  while (str[i] != ' ' && str[i] != 0)
+    i++;
+  if (str[i] == 0)
+    return (-1);
+  i++;
+  if (str[i] == 'r')
+    return (-1);
+  else if (str[i] == LABEL_CHAR)
+    i++;
+  indirect(param, &str[i], i, cmd);
+  if ((i = next(str, i)) == -1)
+    return (-1);
+  if (str[i] == 'r')
+    return (-1);
+  else if (str[i] == LABEL_CHAR)
+    i++;
+  indirect(param, &str[i], i, cmd);
+  if ((i = next(str, i)) == -1)
+    return (-1);
+  if (str[i] != 'r')
+    return (-1);
+  registre(param, str, i, cmd);
+}
