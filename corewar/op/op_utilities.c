3a99672b47abce9baf8171c0a2b4110f827d2329
Tastyep <luc.sinet@epitech.eu>
2013-01-31 17:42:55 +0100

    added some stuff

--- corewar/op/op_utilities.c
+++ corewar/op/op_utilities.c
@@ -5,7 +5,7 @@
 ** Login   <moriss_h@epitech.net>
 **
 ** Started on  Mon Oct  8 09:34:29 2012 hugues morisset
-** Last update Tue Jan 29 16:42:42 2013 luc sinet
+** Last update Thu Jan 31 03:45:50 2013 luc sinet
 */
 
 #include	"operation.h"
