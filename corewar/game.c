6f1172db9073f982e9d34111de767b3fa1fe0bf2
izissise <hugues.m@hotmail.fr>
2013-02-04 12:51:27 +0100

    Decrement cycle_to_die % nbr_live

--- corewar/game.c
+++ corewar/game.c
@@ -101,7 +101,7 @@ int	handle_game(t_vm *vm, char **env)
 
   end_game = 0;
   vm->cycle_count++;
-  if (vm->nbr_live >= NBR_LIVE)
+  if ((vm->nbr_live % NBR_LIVE) == 0)
     vm->cycle_to_die -= CYCLE_DELTA;
   if (vm->flag == 0)
     if ((vm->cycle_to_die != 0) && (vm->cycle_count % vm->cycle_to_die) == 0)
