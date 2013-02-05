c6c9b275167badadfe098e7760f6e607e7c19408
izissise <hugues.m@hotmail.fr>
2013-02-05 16:21:30 +0100

    Add a func to check defines

--- corewar/main.c
+++ corewar/main.c
@@ -10,6 +10,27 @@
 
 #include	"include.h"
 
+int	check_define()
+{
+  if (MEM_SIZE < 1)
+    return (0);
+  if (IDX_MOD < 1)
+    return (0);
+  if (REG_NUMBER < 1)
+    return (0);
+  if ((IND_SIZE != 2) || (REG_SIZE != 4) || (DIR_SIZE != 4))
+    return (0);
+  if (MAX_ARGS_NUMBER != 4)
+    return (0);
+  if ((T_IND != 4) || (T_REG != 1) || (T_DIR != 2))
+    return (0);
+  if ((NBR_LIVE < 1) || (CYCLE_TO_DIE < 0) || (CYCLE_DELTA < 0))
+    return (0);
+  if ((PROG_NAME_LENGTH < 0) || (COMMENT_LENGTH < 0))
+    return (0);
+  return (1);
+}
+
 void	delete_champ_tab(t_champ **tab)
 {
   int	i;
@@ -35,17 +56,20 @@ void	free_all(t_vm *vm)
 int	main(int argc, char **argv, char **envp)
 {
   t_vm	vm;
-  char	**args;
 
-  args = &(argv[1]);
-  pre_pars(args, envp, argc - 1);
-  init_vm_stats(CYCLE_TO_DIE, -1, &vm);
-  if (pars(&(argv[1]), argc - 1, &vm) != -1)
-    while (handle_game(&vm, envp) == 0)
-      {
-        my_apply_on_list(vm.process_list, &exe_process, &vm);
-        usleep(1000);
-      }
-  free_all(&vm);
+  if (check_define())
+    {
+      pre_pars(&(argv[1]), envp, argc - 1);
+      init_vm_stats(CYCLE_TO_DIE, -1, &vm);
+      if (pars(&(argv[1]), argc - 1, &vm) != -1)
+        while (handle_game(&vm, envp) == 0)
+          {
+            my_apply_on_list(vm.process_list, &exe_process, &vm);
+            usleep(1000);
+          }
+      free_all(&vm);
+    }
+  else
+    my_putstr("A define is wrong\n", 1, -1);
   return (0);
 }
