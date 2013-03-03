6d7f7bfa196d6d0bcf1205491e9aa5ddaff8b57f
izissise <hugues.m@hotmail.fr>
2013-03-03 17:59:13 +0100

    Obvious change

--- corewar/game.c
+++ corewar/game.c
@@ -101,7 +101,10 @@ int	handle_game(t_vm *vm, char **env)
   end_game = 0;
   vm->cycle_count++;
   if (((vm->nbr_live % NBR_LIVE) == 0) && (vm->nbr_live != 0))
-    vm->cycle_to_die -= CYCLE_DELTA;
+    {
+      vm->cycle_to_die -= CYCLE_DELTA;
+      vm->nbr_live++;
+    }
   if (vm->flag == 0)
     if ((vm->cycle_to_die != 0) && (vm->cycle_count % vm->cycle_to_die) == 0)
       if (check_champs_alive_a_print(vm) == 1)
